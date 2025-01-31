ff18f546ab6ae33526d2431ab86d1eb7a51e6abe
ravinx <bond4uravii@gmail.com>
2025-01-31 11:09:12 +0100

    update kind cluster script

--- kind-cluster.sh
+++ kind-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 KUBERNETES_VERSION_IMAGE="kindest/node:v1.32.0"
-KUBERNETES_CLUSTER_NAME="dev-cluster"
+KUBERNETES_CLUSTER_NAME="dev"
 KUBERNETES_CLUSTER_CONFIG_PATH="$HOME/.kube/config"
 
 kind create cluster --name $KUBERNETES_CLUSTER_NAME --config kind-config.yaml --image $KUBERNETES_VERSION_IMAGE
